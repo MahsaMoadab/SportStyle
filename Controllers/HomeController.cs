b4ac23afb6774583c9df613b7ee9dabec1dfe817
MahsaMoadab <mahsa.moadab.s@gmail.com>
2021-06-17 12:36:55 +0430

    add projct

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace finalproject.Controllers
+{
+    public class HomeController : Controller
+    {
+       /* private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+       */
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
+    }
+}
